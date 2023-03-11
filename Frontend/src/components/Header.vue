c60a8ec28c8c381b159de61538ffe50276490fbd
Cesar Cortez <cctez36@gmail.com>
2023-03-10 19:23:22 -0600

    Add basic header. Nonworking buttons

---
+++ Frontend/src/components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="d-flex">
+        <v-toolbar>
+            <v-toolbar-title>
+                <div>
+                    <v-btn flat>Hisway African Grocery Store</v-btn>
+                </div>
+
+            </v-toolbar-title>
+            <v-toolbar-items>
+                <v-btn flat>
+                    <div class="d-flex mr-1">
+                        <v-icon icon="mdi-cart"></v-icon>
+                    </div>
+                    <span>Manage Inventory</span>
+                </v-btn>
+                <v-btn flat>
+                    <div class="d-flex mr-1">
+                        <v-icon icon="mdi-account-tie"></v-icon>
+                    </div>
+                    <span>Manage Employees</span>
+                </v-btn>
+                <v-btn flat>
+                    <div class="d-flex mr-1">
+                        <v-icon icon="mdi-star"></v-icon>
+                    </div>
+                    <span>Customer Reviews</span>
+                </v-btn>
+            </v-toolbar-items>
+
+
+        </v-toolbar>
+    </div>
+</template>
