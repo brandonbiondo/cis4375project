4cf81e131e9587c834c5fff300b9789ed1276311
amolskiCIS <amolski@cougarnet.uh.edu>
2023-04-06 20:55:11 -0500

    Major Update, first build
    Still building the homepage, but much has been done. Main things still missing are testimonial display and contact pieces. Other things are minor tweaks and changes.

--- Frontend/src/views/HomeView.vue
+++ Frontend/src/views/HomeView.vue
@@ -1,8 +1,142 @@
 <script>
+import Header from "./components/Header.vue";
 </script>
 
 <template>
-      <h1>
-      Hisway African Grocery Store
-    </h1>
+<div class="homepage">
+<head>
+    <span class="store_title">Hisway African Grocery Store</span></head>
+  <body max-width="1000px" class="mx-auto">
+    <div>
+    <div class="Main">
+      <div>
+        <p>Grocery Store in Katy<br>Open today until 8:00PM</p>
+        <button class="button">GET QUOTE</button>
+        <p class="image"><img src="https://lh3.googleusercontent.com/p/AF1QipNVh7_zlLdspQ1mXFGFsvZdv64oUX9qB0d-q47k=w1080-h608-p-no-v0"></p>
+</div>
+</div>
+    <div class="titles">
+      <h3>TESTIMONIALS</h3>
+      <div class="grid-container">
+    <div class="col-sm-4">
+  </div>
+  <div class="col-sm-4">
+  </div>
+  <div class="col-sm-4">
+  </div>
+    </div>
+      <a href="https://www.google.com/search?hl=en-US&gl=us&q=Hisway+African+Grocery+Store,+3507+Paintbrush+Dawn+Ct,+Katy,+TX+77493&ludocid=2196565002355305937&lsig=AB86z5VTmhcTAe3N-Wg0DsFynK9t#lrd=0x864127667b37dced:0x1e7bc43b53716dd1,3" class="links">WRITE A REVIEW</a>
+      <a href="https://www.google.com/search?hl=en-US&gl=us&q=Hisway+African+Grocery+Store,+3507+Paintbrush+Dawn+Ct,+Katy,+TX+77493&ludocid=2196565002355305937&lsig=AB86z5VTmhcTAe3N-Wg0DsFynK9t#lrd=0x864127667b37dced:0x1e7bc43b53716dd1,1" class="links">READ MORE</a>
+  </div>
+    <div class="titles">
+      <h3>GALLERY</h3>
+     <div class="grid-container">
+      <div class="grid-item">
+        <a href="https://www.google.com/maps/uv?pb=!1s0x864127667b37dced%3A0x1e7bc43b53716dd1!3m1!7e131!4s!5sHisway%20African%20Grocery%20Store!15sCgIgAQ&hl=en&imagekey=!1e10!2sAF1QipPKz1PFfWK-srKrEnTrTYSKBwLeiTtZ-_SnXvL6#">
+          <img src="https://lh3.googleusercontent.com/p/AF1QipPKz1PFfWK-srKrEnTrTYSKBwLeiTtZ-_SnXvL6=w960-h960-n-o-v1" style="width:300px;height:300px;">
+        </a>
+  </div>
+  <div class="grid-item">
+    <a href="https://www.google.com/maps/uv?pb=!1s0x864127667b37dced%3A0x1e7bc43b53716dd1!3m1!7e131!4s!5sHisway%20African%20Grocery%20Store!15sCgIgAQ&hl=en&imagekey=!1e10!2sAF1QipOCxqi4mvlVybSpt1kn7osBK4n-IEvxB2CUF88E">
+      <img src="https://lh3.googleusercontent.com/p/AF1QipOCxqi4mvlVybSpt1kn7osBK4n-IEvxB2CUF88E=w960-h960-n-o-v1" style="width:300px;height:300px;">
+    </a>
+    </div>
+    <div class="grid-item">
+      <a href="https://www.google.com/maps/uv?pb=!1s0x864127667b37dced%3A0x1e7bc43b53716dd1!3m1!7e131!4s!5sHisway%20African%20Grocery%20Store!15sCgIgAQ&hl=en&imagekey=!1e10!2sAF1QipNomBKG0GeMbTiJrA-tjKFPNLaiAQrPwQ5SpfCD">
+      <img src="https://lh3.googleusercontent.com/p/AF1QipNomBKG0GeMbTiJrA-tjKFPNLaiAQrPwQ5SpfCD=w960-h960-n-o-v1" style="width:300px;height:300px;">
+    </a>
+  </div>
+      <div class="grid-item">
+        <a href="https://www.google.com/maps/uv?pb=!1s0x864127667b37dced%3A0x1e7bc43b53716dd1!3m1!7e131!4s!5sHisway%20African%20Grocery%20Store!15sCgIgAQ&hl=en&imagekey=!1e10!2sAF1QipOlN1C5coX7LhxtGlx2rotbmFd0E5Kn4axCTST7">
+      <img src="https://lh3.googleusercontent.com/p/AF1QipOlN1C5coX7LhxtGlx2rotbmFd0E5Kn4axCTST7=w960-h960-n-o-v1" style="width:300px;height:300px;">
+    </a>
+  </div>
+  <div class="grid-item">
+    <a href="https://www.google.com/maps/uv?pb=!1s0x864127667b37dced%3A0x1e7bc43b53716dd1!3m1!7e131!4s!5sHisway%20African%20Grocery%20Store!15sCgIgAQ&hl=en&imagekey=!1e10!2sAF1QipPCx2YAfr6WGRdiA-xyvhzMoZnKFw10oEFYOkRS">
+      <img src="https://lh3.googleusercontent.com/p/AF1QipPCx2YAfr6WGRdiA-xyvhzMoZnKFw10oEFYOkRS=w960-h960-n-o-v1" style="width:300px;height:300px;">
+    </a>
+    </div>
+    <div class="grid-item">
+      <a href="https://www.google.com/maps/uv?pb=!1s0x864127667b37dced%3A0x1e7bc43b53716dd1!3m1!7e131!4s!5sHisway%20African%20Grocery%20Store!15sCgIgAQ&hl=en&imagekey=!1e10!2sAF1QipOzL77eBO7ru0AEYVyOlOiktMLfceecTPaUSvh8">
+      <img src="https://lh3.googleusercontent.com/p/AF1QipOzL77eBO7ru0AEYVyOlOiktMLfceecTPaUSvh8=w960-h960-n-o-v1" style="width:300px;height:300px;">
+    </a>
+  </div>
+      <div class="grid-item">
+        <a href="https://www.google.com/maps/uv?pb=!1s0x864127667b37dced%3A0x1e7bc43b53716dd1!3m1!7e131!4s!5sHisway%20African%20Grocery%20Store!15sCgIgAQ&hl=en&imagekey=!1e10!2sAF1QipNbqIZWnsQgYUQEx6e05Z3jyp6PE0p4zGCzutDj">
+      <img src="https://lh3.googleusercontent.com/p/AF1QipNbqIZWnsQgYUQEx6e05Z3jyp6PE0p4zGCzutDj=w960-h960-n-o-v1" style="width:300px;height:300px;">
+    </a>
+  </div>
+  <div class="grid-item">
+    <a href="https://www.google.com/maps/uv?pb=!1s0x864127667b37dced%3A0x1e7bc43b53716dd1!3m1!7e131!4s!5sHisway%20African%20Grocery%20Store!15sCgIgAQ&hl=en&imagekey=!1e10!2sAF1QipOssy02-DQavUtlA-JTmd25GIn-dkgGH_AJ2pni">
+      <img src="https://lh3.googleusercontent.com/p/AF1QipOssy02-DQavUtlA-JTmd25GIn-dkgGH_AJ2pni=w960-h960-n-o-v1" style="width:300px;height:300px;">
+    </a>
+    </div>
+    <div class="grid-item">
+      <a href="https://www.google.com/maps/uv?pb=!1s0x864127667b37dced%3A0x1e7bc43b53716dd1!3m1!7e131!4s!5sHisway%20African%20Grocery%20Store!15sCgIgAQ&hl=en&imagekey=!1e10!2sAF1QipOviEoODp5ALa751KGQqENaK5dQjp0nLwjn8LUH">
+      <img src="https://lh3.googleusercontent.com/p/AF1QipOviEoODp5ALa751KGQqENaK5dQjp0nLwjn8LUH=w960-h960-n-o-v1" style="width:300px;height:300px;">
+    </a>
+  </div>
+      </div>
+  </div>
+    <div class="titles">
+    <div>
+      <h3>CONTACTS</h3>
+    </div>
+  </div>
+  </div>
+</body>
+</div>
 </template>
+
+<style>
+.homepage {
+  max-width: 1000px;
+  margin: auto;
+  text-align: center;
+}
+.grid-container {
+  display: grid;
+  grid-template-columns: auto auto auto;
+  width: 960px;
+  height: 960px;
+  align-content: space-evenly;
+}
+
+.button {
+  color: white;
+  background-color: #5f9bce;
+  font-size: 17px;
+  height: 60px;
+  width: 190px;
+  display: inline-block;
+  font-family: "Open Sans",sans-serif;
+  border:none;
+  margin: 15px;
+}
+.store_title {
+  font-family: "Abril FatFace",serif;
+  color: #5f9bce;
+  font-size: 56px;
+  font-weight: bold;
+}
+.image {
+  text-align: center;
+  display: block;
+}
+.titles{
+  font-family: "Open Sans",sans-serif;
+  font-weight: bold;
+  padding: 10px;
+  border: 10px;
+}
+.links{
+  font-family: "Open-Sans",sans-serif;
+  color: #5f9bce;
+  margin: 15px;
+  border-bottom: 1px solid;
+  font-weight: 550;
+  padding-bottom: 8px;
+  text-align: center;
+  text-decoration: none;
+}
+</style>
